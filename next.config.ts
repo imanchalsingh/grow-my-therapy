1333c93e6e8899eb2095494aeabbd4653806d6cb
imanchalsingh <singhanchal159357@gmail.com>
2026-02-10 11:44:50 +0530

    fix :next config domain error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "careersinpsychology.org",
+      },
+      {
+        protocol: "https",
+        hostname: "summitcounseling.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.squarespace-cdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
