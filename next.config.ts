6dcfab3e9083fd0760697df303581c00b429b418
imanchalsingh <singhanchal159357@gmail.com>
2026-02-10 15:04:20 +0530

    added domain in next config

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.squarespace-cdn.com",
       },
+      {
+        protocol: "https",
+        hostname: "wallpaperbat.com",
+      },
     ],
   },
 };
